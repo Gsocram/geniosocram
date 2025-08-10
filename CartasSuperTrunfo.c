5aa023df2b1c929953bca318247eb6bfd1534994
geniosocram <marcoskaio0171@gmail.com>
2025-08-10 23:00:10 +0000

    creie printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,8 @@ int populacao = 2568900;
 float area = 150.75;
 float pib = 6290000000.0f;
 int numerodepontosturisticos = 100;
+
+printf("Digite o Estado: %c\n", Estado);
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
